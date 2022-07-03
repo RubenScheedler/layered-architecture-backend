40658ae5309f1cd8ad4b3ec342007c06b7fb7bd7
ruben <r.j.scheedler@student.rug.nl>
2022-07-03 10:34:09 +0200

    Add spring initializr generated sources

---
+++ src/main/java/com/hashnode/rubenscheedler/layeredArchitectureBackend/LayeredArchitectureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hashnode.rubenscheedler.layeredArchitectureBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredArchitectureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayeredArchitectureBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hashnode/rubenscheedler/layeredArchitectureBackend/LayeredArchitectureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashnode.rubenscheedler.layeredArchitectureBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayeredArchitectureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
